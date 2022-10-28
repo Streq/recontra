5038fb22ad98e285f648013b49d615e6f10dc20f
streq <strequena@gmail.com>
2022-10-28 14:13:29 -0300

    listo me parece que ya tamo

--- src/shooter/dead.gd
+++ src/shooter/dead.gd
@@ -2,6 +2,6 @@ extends FighterState
 
 func _physics_update(delta: float):
 	if !root.is_on_floor():
-		goto("air_dead")
+		goto("dead_air")
 		return
 	root.velocity.x = lerp(root.velocity.x, 0.0, root.idle_lerp*delta)
